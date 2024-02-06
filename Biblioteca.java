e7430e9f96d9fccda1f350a55083798b541f581a
Netella23 <152686094+Netella23@users.noreply.github.com>
2024-02-06 22:37:22 +0100

    Add files via upload

---
+++ Biblioteca.java
@@ -0,0 +1,187 @@
+package gestione;
+
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Biblioteca {
+	private Libro[] libri;
+	public enum DatiLibro {Titolo, Autore, Editore, Numero_Pagine, Codice_ISBN, Prezzo, Data_Pubblicazione};
+	
+	public Biblioteca() {
+		this.libri = new Libro[50];
+	}
+	
+	public void aggiungiLibro(Libro libro) {
+		int i = 0;
+		boolean inserito = false;
+		
+		while(i < libri.length && !inserito){
+			if(libri[i] == null) {
+				libri[i] = libro;
+				inserito = true;
+				System.out.println("Libro inserito correttamente");
+			}
+			i++;
+		}
+	}
+	
+	public void modificaLibro(int numLibro) {
+		Scanner scanner = new Scanner(System.in);
+		int scelta;
+		double prezzo;
+		String modifica;
+		DatiLibro[] dati = DatiLibro.values(); 
+		LocalDate modificaData;
+		System.out.println("Cosa vuoi modificare del libro " + libri[numLibro].getTitolo() + "?");
+		for(int i=0; i<DatiLibro.values().length; i++)
+			System.out.println(i + "-" + dati[i].name());
+		scelta = scanner.nextInt();
+		
+		switch(scelta) {
+			case 1:
+				System.out.println("Modifica titolo:");
+				modifica = scanner.nextLine();
+				libri[numLibro].setTitolo(modifica);
+				break;
+			case 2: 
+				System.out.println("Modifica autore:");
+				modifica = scanner.nextLine();
+				libri[numLibro].setAutore(modifica);
+				break;
+			case 3:
+				System.out.println("Modifica editore:");
+				scanner.nextLine();
+				modifica = scanner.nextLine();
+				libri[numLibro].setEditore(modifica);
+				break;
+			case 4:
+				System.out.println("Modifica numero pagine:");
+				scelta = scanner.nextInt();
+				libri[numLibro].setNumPagine(scelta);
+				break;
+			case 5:
+				System.out.println("Modifica codice ISBN:");
+				scelta = scanner.nextInt();
+				libri[numLibro].setCodISBN(scelta);
+				break;
+			case 6:
+				prezzo = scanner.nextDouble();
+				System.out.println("Modifica prezzo:");
+				libri[numLibro].setPrezzo(prezzo);
+				break;
+			case 7:
+				System.out.println("Modifica data pubblicazione: (formato gg/mm/aaaa)");
+				scanner.nextLine();
+				String data = scanner.nextLine();
+				LocalDate data1 = dateInput(data);
+				libri[numLibro].setDataPubblicazione(data1);
+				break;
+		}
+		
+	}
+	
+	public static LocalDate dateInput(String userInput) {
+
+	    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    LocalDate date = LocalDate.parse(userInput, dateFormat);
+
+	    return date;
+	}
+	
+	private static void salvaLibriInFile(Libro[] libri) {
+        String percorsoFile = "databaseBiblioteca.txt"; 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(percorsoFile))) {
+            for (Libro libro : libri) {
+        
+                if (libro != null) {
+                    writer.write(libro.toString());
+                    writer.newLine(); 
+                }
+            }
+
+            System.out.println("Libri salvati con successo nel file.");
+        } catch (IOException e) {
+            System.err.println("Errore durante la scrittura nel file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+	
+	
+	
+	public static void main(String[] args) {
+		Biblioteca biblioteca = new Biblioteca();
+		int scelta;
+		Scanner scanner = new Scanner(System.in);
+		do {
+			System.out.println("1- Aggiungi un libro");
+			System.out.println("2- Modifica un libro");
+			System.out.println("3- Rimuovi un libro");
+			System.out.println("4- Visualizza un libro");
+			System.out.println("ALTRIMENTI INSERISCI QUALSIASI NUMERO PER USCIRE");
+			
+			scelta = scanner.nextInt();
+			scanner.nextLine();
+			
+			switch(scelta) {
+				case 1:
+					System.out.println("Titolo:");
+					String titolo = scanner.nextLine();
+					System.out.println("Autore:");
+					String autore = scanner.nextLine();
+					System.out.println("Editore:");
+					String editore = scanner.nextLine();
+					System.out.println("Numero pagine:");
+					int pagine = scanner.nextInt();
+					System.out.println("Codice ISBN:");
+					int ISBN = scanner.nextInt();
+					System.out.println("Prezzo:");
+					double prezzo = scanner.nextDouble();
+					System.out.println("Data pubblicazione: (formato gg/mm/aaaa)");
+					scanner.nextLine();
+					String data = scanner.nextLine();
+					LocalDate data1 = dateInput(data);
+					Libro libro = new Libro(titolo,autore,editore,pagine,ISBN,prezzo,data1);
+					biblioteca.aggiungiLibro(libro);
+					break;
+				case 2:
+					int i=0;
+					boolean fine = false;
+					System.out.println("Scegli un libro inserendo il numero:");
+					while(i<biblioteca.libri.length && !fine) {
+						if(biblioteca.libri[i] != null)
+							System.out.println("Libro " + i + "\nTitolo=" + biblioteca.libri[i].getTitolo());
+						else
+							fine = true;
+						i++;
+					}
+					int modifica = scanner.nextInt();
+					biblioteca.modificaLibro(modifica);
+					break;
+				case 3: 
+					break;
+				case 4:
+					boolean end = false;
+					int visualizza = 0, j =0;
+					System.out.println("Scegli il libro da visualizzare:");
+					while(j<biblioteca.libri.length && !end) {
+						if(biblioteca.libri[j] != null)
+							System.out.println("Libro " + j + "\nTitolo=" + biblioteca.libri[j].getTitolo());
+						else
+							fine = true;
+						j++;
+					}
+					visualizza = scanner.nextInt();
+					System.out.println(biblioteca.libri[visualizza]);
+					break;
+			}
+			salvaLibriInFile(biblioteca.libri);
+			
+		}while(scelta<5 && scelta>0);
+	}
+}
---
+++ Libro.java
@@ -0,0 +1,83 @@
+package gestione;
+
+import java.time.LocalDate;
+
+public class Libro extends Biblioteca{
+	private String titolo;
+	private String autore;
+	private String editore;
+	private int numPagine;
+	private int codISBN;
+	private double prezzo;
+	private LocalDate dataPubblicazione;
+	
+	public Libro(String titolo, String autore, String editore, int numPagine, int codISBN, double prezzo, LocalDate dataPubblicazione) {
+			this.titolo = titolo;
+			this.autore = autore;
+			this.editore = editore; 
+			this.numPagine = numPagine;
+			this.codISBN = codISBN;
+			this.prezzo = prezzo;
+			this.dataPubblicazione = dataPubblicazione;
+	}
+	
+	public String getTitolo() {
+		return titolo;
+	}
+	
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	public String getAutore() {
+		return autore;
+	}
+	
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+	
+	public String getEditore() {
+		return editore;
+	}
+	
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+
+	public int getNumPagine() {
+		return numPagine;
+	}
+
+	public void setNumPagine(int numPagine) {
+		this.numPagine = numPagine;
+	}
+
+	public int getCodISBN() {
+		return codISBN;
+	}
+
+	public void setCodISBN(int codISBN) {
+		this.codISBN = codISBN;
+	}
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public LocalDate getDataPubblicazione() {
+		return dataPubblicazione;
+	}
+
+	public void setDataPubblicazione(LocalDate dataPubblicazione) {
+		this.dataPubblicazione = dataPubblicazione;
+	}
+	
+	public String toString() {
+		return ("Titolo: " + titolo + "\nAutore: " + autore + "\nEditore: " + editore + "\nNumero pagine:" + numPagine + "\nCodice ISBN: " + codISBN + "\nPrezzo: " + prezzo + "\nData pubblicazione: " + dataPubblicazione );
+	}
+}
